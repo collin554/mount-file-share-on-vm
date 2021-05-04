826fadddd782d64d0a4edf7cc42f8bd86a40aca7
Collin Lee <clee@productiveedge.com>
2021-05-04 08:35:23 -0500

    add uid gid

--- startupscript.sh
+++ startupscript.sh
@@ -11,10 +11,8 @@ sudo apt install cifs-utils
 resourceGroupName=$4
 storageAccountName=$5
 fileShareName=$6
-
-mntPath="file-share-mount/"
-
-sudo mkdir -p $mntPath
+sftpUserName=$7
+sftpUserPW=$8
 
 httpEndpoint=$(az storage account show \
     --resource-group $resourceGroupName \
@@ -27,4 +25,23 @@ storageAccountKey=$(az storage account keys list \
     --account-name $storageAccountName \
     --query "[0].value" | tr -d '"')
 
-sudo mount -t cifs $smbPath $mntPath -o vers=3.0,username=$storageAccountName,password=$storageAccountKey,serverino
+sudo adduser $sftpUserName
+echo "$sftpUserName:$sftpUserPW" | sudo chpasswd
+
+sudo mkdir -p /home/sftp/uploads
+sudo chown root:root /home/sftp
+sudo chmod 755 /home/sftp
+sudo chown $sfptUserName:$sftpUserName /home/sftp/uploads
+
+echo "Match User $sftpUserName" | sudo tee -a /etc/ssh/sshd_config
+echo "ForceCommand internal-sftp" | sudo tee -a /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+echo "ChrootDirectory /home/sftp" | sudo tee -a /etc/ssh/sshd_config
+echo "PermitTunnel no" | sudo tee -a /etc/ssh/sshd_config
+echo "AllowAgentForwarding no" | sudo tee -a /etc/ssh/sshd_config
+echo "AllowTcpForwarding no" | sudo tee -a /etc/ssh/sshd_config
+echo "X11Forwarding no" | sudo tee -a /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
+
+sudo mount -t cifs $smbPath /home/sftp/uploads -o vers=3.0,username=$storageAccountName,password=$storageAccountKey,uid=$(id -u $sftpUserName),gid=$(id -g $sftpUserName),serverino
